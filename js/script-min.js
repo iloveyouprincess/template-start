89a64db8fb5951c95cce8bfe4678d513b5719a91
ProGirl <me_yura@hotmail.com>
2016-03-04 14:09:34 +0300

    start

---
+++ js/script-min.js
@@ -0,0 +1,1 @@
+function dasdas(){console.log("123123132")}console.log("dasd");var dasd=31;
---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log( "dasd" );
+var dasd = 31;
+
+
+function dasdas( d, a ) {
+    console.log( "123123132" );
+}
+
